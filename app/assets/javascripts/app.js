a149a43bb0937446737a21073a851a3787b6840f
Eric Kwong <erickwong618@yahoo.com>
2016-05-08 01:39:03 -0400

    testing click to trigger tts

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -17,12 +17,17 @@ console.log('app.js!!!');
 //   });
 // }, 5000);
 
-setTimeout(function() {
-  responsiveVoice.speak("You are now entering into a medium accident zone.")
-  notie.alert(2, 'Accident Warning: You are in an accident orange zone.', 4);
-}, 5000);
+$('#bggif').click(function() {
+  var audio = new Audio("my file");
+  audio.play();
+  setTimeout(function() {
+    responsiveVoice.speak("You are now entering into a medium accident zone.")
+    notie.alert(2, 'Accident Warning: You are in an accident orange zone.', 4);
+  }, 5000);
 
-setTimeout(function() {
-  responsiveVoice.speak("You are now entering into a high accident zone.")
-  notie.alert(3, 'Accident Warning: You are in an accident red zone.', 4);
-}, 10000);
+  setTimeout(function() {
+    responsiveVoice.speak("You are now entering into a high accident zone.")
+    notie.alert(3, 'Accident Warning: You are in an accident red zone.', 4);
+  }, 10000);
+  
+});
