54b27ca214ac89ed0f376e0af9dc9245992ed14a
Eric Kwong <erickwong618@yahoo.com>
2016-05-07 23:46:08 -0400

    added notification plugin and code for notification with api

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,17 @@
 console.log('app.js!!!');
+setInterval(function(){
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    $.get('https://bumperz.herokuapp.com/accidents/warn_level', {latitude: latitude, longitude: longitude}).done(function(res) {
+      if (res.warning == "yellow") {
+        notie.alert(2, 'Accident Warning: You are in an accident yellow zone.', 2);
+      } else if (res.warning == "orange") {
+        notie.alert(2, 'Accident Warning: You are in an accident orange zone.', 2);
+      } else if (res.wanring == "red") {
+        notie.alert(3, 'Accident Warning: You are in an accident red zone.', 2);
+      }
+    });
+  });
+}, 5000);
+
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require notie.js
+//= require app.js
